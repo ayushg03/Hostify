360c4fe12d3d663886d85409538eb1a69ba99506
ayushg03 <20cs3016@rgipt.ac.in>
2024-02-13 16:37:52 +0530

    feat: add redis-socket server for logs

--- apps/api-server/index.ts
+++ apps/api-server/index.ts
@@ -1,9 +1,32 @@
 import express, { Request, Response } from 'express';
 import {generateSlug}   from 'random-word-slugs';
 import  {ECSClient, RunTaskCommand}   from '@aws-sdk/client-ecs';
+import Redis from 'ioredis';
+import {Server, Socket} from 'socket.io';
+import http from 'http';
 
 const app = express();
 const PORT: number = 9000;
+
+const subscriber=new Redis('');
+const server = http.createServer(app); // Create an HTTP server instance
+
+const io = new Server(server, { cors: { origin: '*' } }); // Pass the server instance to Socket.IO
+
+io.on('connection',socket=>{
+    socket.on('subscribe',channel=>{
+        socket.join(channel);
+        socket.emit('message',`Joined ${channel}`);
+    })
+})
+
+server.listen(9001, () => {
+    console.log('Socket Server running on 9001');
+});
+
+
+
+
 
 const ecsClient = new ECSClient({
     region: 'ap-south-1',
@@ -18,12 +41,11 @@ const config = {
     TASK: ''
 }
 
-
 app.use(express.json());
 
 app.post('/project',async(req:Request,res:Response)=>{
-    const { gitURL } = req.body
-    const projectSlug = generateSlug();
+    const { gitURL,slug } = req.body
+    const projectSlug =slug? slug: generateSlug();
     
     console.log(gitURL);
     //spin the container
@@ -57,5 +79,16 @@ app.post('/project',async(req:Request,res:Response)=>{
     return res.json({ status: 'queued', data:{projectSlug, url:`http:\\${projectSlug}.localhost:8000`}})
 
 })
+
+function initRedisSubscribe() {
+  console.log("Subscribed to logs...")
+  subscriber.psubscribe('logs:*');
+  subscriber.on('pmessage', (pattern, channel, message) => {
+    io.to(channel).emit('message', message);
+});
+
+}
+
+initRedisSubscribe();
 
 app.listen(PORT, () => console.log(`API Server Running..${PORT}`));
