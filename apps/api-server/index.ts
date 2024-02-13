7564c5f39f32e8f485c8ea943e79cb4cdfca109a
ayushg03 <20cs3016@rgipt.ac.in>
2024-02-13 12:14:07 +0530

    feat: add api-server to apin the container

---
+++ apps/api-server/index.ts
@@ -0,0 +1,60 @@
+import express, { Request, Response } from 'express';
+import {generateSlug}   from 'random-word-slugs';
+import  {ECSClient, RunTaskCommand}   from '@aws-sdk/client-ecs';
+
+const app = express();
+const PORT: number = 9000;
+
+const ecsClient = new ECSClient({
+    region: 'ap-south-1',
+    credentials: {
+        accessKeyId: '',
+        secretAccessKey: ''
+    }
+})
+
+const config = {
+    CLUSTER: '',
+    TASK: ''
+}
+
+app.use(express.json());
+
+app.post('/project',async(req:Request,res:Response)=>{
+    const { gitURL } = req.body
+    const projectSlug = generateSlug();
+    
+    console.log(gitURL);
+    //spin the container
+    const command =  new RunTaskCommand({
+        cluster: config.CLUSTER,
+        taskDefinition: config.TASK,
+        launchType: 'FARGATE',
+        count: 1,
+        networkConfiguration: {
+            awsvpcConfiguration: {
+                assignPublicIp: 'ENABLED',
+                subnets: [],
+                securityGroups: []
+            }
+        },
+        overrides: {
+            containerOverrides: [{
+                name: 'builder-image',
+                environment: [
+                    {name: 'GIT_REPOSITORY_URL', value:gitURL},
+                    {name: 'PROJECT_ID', value:projectSlug },
+                    {name: 'ACCESS_KEY_ID', value: ''},
+                    {name: 'SECRET_ACCESS_KEY', value: '' }
+                ]
+            }]
+        } 
+    })
+
+    await ecsClient.send(command);
+    
+    return res.json({ status: 'queued', data:{projectSlug, url:`http:\\${projectSlug}.localhost:8000`}})
+
+})
+
+app.listen(PORT, () => console.log(`API Server Running..${PORT}`));
